b4033d29116e23a30251ae484711d2e3cc6e058c
Shyheim Russell <srussell@olin.edu>
2020-05-06 14:04:25 -0400

    All plots in time domain created

--- Final MiniProject/modem_rx_starter.m
+++ Final MiniProject/modem_rx_starter.m
@@ -22,15 +22,31 @@ t = [0:length(y_t)-1]';
 y_c = y_t.*cos(2*pi*f_c*t/Fs);
 
 filter_y = lowpass(y_c, .2);
-plot(filter_y)
+subplot(2,2,1)
+plot(y_t)
+xlabel('Time (seconds)');
+ylabel('Y_t')
+subplot(2,2,2)
+plot(t, y_c)
+xlabel('Time (seconds)');
+ylabel('Y_c');
+subplot(2,2,[3,4])
+plot(t, filter_y)
+xlabel('Time (seconds)');
+ylabel('Filtered Y_c');
 
 %%
-vec_y = filter_y(50:100:4000,:)
-vec_y_bits = (filter_y(50:100:4000,:)>=0)
+t_vec = [0:length(vec_y)-1];
+vec_y = filter_y(50:100:4000,:);
+vec_y_bits = (filter_y(50:100:4000,:)>=0);
 subplot(2,1,1)
-stem(vec_y)
-subplot(2, 1,2)
-stem(vec_y_bits)
+stem(t_vec, vec_y)
+xlabel('Time (seconds)');
+ylabel('Filtered Y_c')
+subplot(2,1,2)
+stem(t_vec, vec_y_bits)
+xlabel('Time (seconds)');
+ylabel('Binary Filtered Y_c')
 
 
 word = BitsToString(double(vec_y_bits))
@@ -48,17 +64,36 @@ start_idx = find_start_of_signal(y_r,x_sync);
 y_t = y_r(start_idx+length(x_sync):end); % y_t is the signal which starts at the beginning of the transmission
 
 %%
-
-plot(y_t)
 t = [0:length(y_t)-1]';
 y_c = y_t.*cos(2*pi*f_c*t/Fs);
 filter_y = lowpass(y_c, .2);
+subplot(2,2,1)
+plot(y_t)
+xlabel('Time (seconds)');
+ylabel('Y_t')
+subplot(2,2,2)
+plot(t, y_c)
+xlabel('Time (seconds)');
+ylabel('Y_c');
+subplot(2,2,[3,4])
+plot(t, filter_y)
+xlabel('Time (seconds)');
+ylabel('Filtered Y_c');
+%%
+t_vec = [0:length(vec_y)-1];
 vec_y = filter_y(50:100:(123*8*100),:)
 vec_y_bits = (filter_y(50:100:(123*8*100),:)>=0)
 
 mystery = BitsToString(double(vec_y_bits))
 
-
+subplot(2,1,1)
+stem(t_vec, vec_y)
+xlabel('Time (seconds)');
+ylabel('Filtered Y_c')
+subplot(2,1,2)
+stem(t_vec, vec_y_bits)
+xlabel('Time (seconds)');
+ylabel('Binary Filtered Y_c')
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
