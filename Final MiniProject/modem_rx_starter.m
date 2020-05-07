80d4796a8a4c56b7f3c6fc71e54080beccf7524b
Sabrina Pereira <spereira@olin.edu>
2020-05-07 12:39:00 -0500

    Test for frequency plots

--- Final MiniProject/modem_rx_starter.m
+++ Final MiniProject/modem_rx_starter.m
@@ -35,6 +35,36 @@ plot(t, filter_y)
 xlabel('Time (seconds)');
 ylabel('Filtered Y_c');
 
+%% 
+
+data = filter_y %(50:4000,:)
+T = 1/Fs;             % Sampling period       
+L = length(data);     % Length of signal
+t = (0:L-1)*T;        % Time vector
+
+Y = fft(data);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L/2))/L;
+
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of S(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+% n = length(data); 
+% Ts = t(2)-t(1); % sample time
+% Fs = 1/Ts; % sampling frequency
+% NFFT = 2^nextpow2(n); % Next power of 2 from length of data
+% Y = fft(data,NFFT)/n;
+% f = Fs/2*linspace(0,1,NFFT/2+1);
+% Plot single-sided amplitude spectrum.
+% plot(f,2*abs(Y(1:NFFT/2+1))) 
+% title('Single-Sided Amplitude Spectrum of y(t)')
+% xlabel('Frequency (Hz)')
+% ylabel('|Y(f)|')
+
 %%
 vec_y = filter_y(50:100:4000,:);
 vec_y_bits = (filter_y(50:100:4000,:)>=0);
